bff69910cc9172938f3b9e12e16074e509e126c0
Nobby-code <norbertguda0@gmail.com>
2024-01-28 11:20:41 +0300

    react props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Ninja from "./components/Ninja";
+
 function App() {
   return (
     <div className="App">
       <h1>My New React App!</h1>
       <p>Welcome :)</p>
+      <Ninja name="Nobby" age="25" belt="Black" />
+      <Ninja name="Guda" age="30" belt="Black" />
     </div>
   );
 }
---
+++ src/components/Ninja.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Ninja extends Component {
+  render() {
+    const { name, age, belt } = this.props
+    return (
+      <div>
+         <div>Name: {name}</div>
+         <div>Age: {age}</div>
+         <div>Belt: {belt}</div>
+      </div>
+    )
+  }
+}
